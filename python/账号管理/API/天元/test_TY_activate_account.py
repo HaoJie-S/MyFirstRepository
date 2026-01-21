183ad7b33f4d570492e75b62abfc6210a318c879
Haojie-S <3171796824@qq.com>
2026-01-21 13:53:24 +0800

    Changes

---
+++ python/账号管理/API/天元/test_TY_activate_account.py
@@ -0,0 +1,27 @@
+import requests
+
+
+# 测试查询所有的账号
+class TestAccount:
+    def test_activate_accounts(self, auth_token):
+        """测试查询所有账号"""
+        url = "https://s.kplgnss.com/wxw/api/web/v2/v2/activeacc"
+        json_data = {
+            "diffAccount": "bjxt022"
+        }
+        # headers = {"token": auth_token}
+        headers = {"token": "9F7AC3995E176AD9CA"}
+
+        response = requests.post(url, json=json_data, headers=headers)
+        result = response.json()
+
+        print(f"\n📊 天元位置服务-激活账号结果:")
+        print(f"  状态码: {response.status_code}")
+        print(f"  业务码: {result.get('code')}")
+        print(f"  信息: {result['msg']}")
+        print(f"  详细信息: {result['msgerr']}")
+
+        # if 'data' in result:
+        #     print(f"  数据条数: {len(result['list'])}")
+
+        assert response.status_code == 200
